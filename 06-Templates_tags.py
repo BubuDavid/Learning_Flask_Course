fe1e66527ce89622a32ac78c669439138cbd7b7a
Bubu <david.pedroza.segoviano@gmail.com>
2019-08-11 13:18:41 -0500

    06-Templates_tags

--- 06-Templates_tags.py
+++ 06-Templates_tags.py
@@ -4,17 +4,20 @@ from flask import Flask
 #Importr render:
 from flask import render_template
 
-
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return "Hello men go to localhost:8000/user/name"
 
 #Todo está en html
 @app.route('/user/<name>')
 def user(name = "Bubu"):
-	age = 10
-	my_list = [1, 2, 3, 4]
+	age = 18
+	my_list1 = [1, 2, 3, 4]
 
 	return render_template('06-user.html', name = name, age = age, lista = my_list)
 
 
 if __name__ == '__main__': 
-	app.run(debug = True)
+	app.run(debug = True, port = 8000)
